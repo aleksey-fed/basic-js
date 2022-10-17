5dcaeb5ba8bcdf19fc9d59b9c64b8bd2e2c2b2d2
Aleksey Fedorchenko <alekseyfromperm@gmail.com>
2022-10-17 17:05:58 +0500

    add Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,98 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
+  regexp = /[a-z]/gi
+
+  constructor(direct) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, keyWord) {
+    if (!string || !keyWord) throw new Error('Incorrect arguments!');
+
+    let keyWordArray = [];
+    let index = 0;
+
+    for (let i = 0; i < string.length; i++) {    
+        if (index === keyWord.length) {
+          index = 0;
+        }
+
+        if (string[i].match(this.regexp)) {
+          keyWordArray.push(keyWord[index].toUpperCase());
+          index++;
+        } else {
+          keyWordArray.push(string[i]);
+        }
+    }
+
+    // find result
+    let result = [];
+
+    for (let i = 0; i < string.length; i++) {
+      if (string[i].match(this.regexp)) {
+        let index1 = this.alphabet.indexOf(string[i].toUpperCase());
+        let index2 = this.alphabet.indexOf(keyWordArray[i].toUpperCase());
+
+        let index = index1 + index2;
+
+        if (index >= 25) {
+          index = index % 26;
+        }
+        result.push(this.alphabet[index]);
+      } else {
+        result.push(string[i]);
+      }
+    }
+
+    this.direct === false ? result = result.reverse().join('') : result = result.join('');
+
+    return result;    
+  }
+
+  decrypt(string, keyWord) {
+    if (!string || !keyWord) throw new Error('Incorrect arguments!');
+
+    let keyWordArray = [];
+    let index = 0;
+
+    for (let i = 0; i < string.length; i++) {    
+      if (index === keyWord.length) {
+        index = 0;
+      }
+
+      if (string[i].match(this.regexp)) {
+        keyWordArray.push(keyWord[index].toUpperCase());
+        index++;
+      } else {
+        keyWordArray.push(string[i]);
+      }
+    }
+
+    // find result
+    let result = [];
+
+    for (let i = 0; i < string.length; i++) {
+      if (string[i].match(this.regexp)) {
+        let index1 = this.alphabet.indexOf(string[i].toUpperCase());
+        let index2 = this.alphabet.indexOf(keyWordArray[i].toUpperCase());
+  
+        let index = index1 - index2;
+  
+        if (index < 0) {
+          index = 26 - Math.abs(index);
+        }
+        
+        result.push(this.alphabet[index]);
+      } else {
+        result.push(string[i]);
+      }
+    }
+
+    this.direct === false ? result = result.reverse().join('') : result = result.join('');
+
+    return result;
   }
 }
 
